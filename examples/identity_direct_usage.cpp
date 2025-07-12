49913bf02c93cad6c071075f4f6fb7c45e85f6d0
Jeff Garland <jeff@crystalclearsoftware.com>
2025-07-12 15:31:52 -0700

    fix examples

--- examples/identity_direct_usage.cpp
+++ examples/identity_direct_usage.cpp
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include <beman/exemplar/identity.hpp>
+#include <beman/cache_latest/cache_latest.hpp>
 
 #include <iostream>
 
-namespace exe = beman::exemplar;
-
 int main() {
-    std::cout << exe::identity()(2024) << '\n';
+    std::cout << "hello" << '\n';
     return 0;
 }
